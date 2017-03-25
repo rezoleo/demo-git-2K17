4f9b58e792626e4ea32aa1eba9586bd4ec574e81
Florian Heymes <florianheymes@gmail.com>
2017-03-25 17:28:59 +0100

    main

--- main.rb
+++ main.rb
@@ -3,7 +3,8 @@ require "./DivisibleParCinq"
 require "./DivisibleParQuinze"
 
 100.each do | number|
-print number
-if (divParQuinze(number) = True ) do puts "Fiz Buz" 
-elsif (divParCinq(number) = True ) do puts "Buz" 
-elsif (divParTrois(number) = True ) do puts "Fiz" 
+	print number
+	if (divParQuinze(number) = True ) do puts "Fiz Buz"; end
+	elsif (divParCinq(number) = True ) do puts "Buz"; end
+	elsif (divParTrois(number) = True ) do puts "Fiz"; end 
+end
