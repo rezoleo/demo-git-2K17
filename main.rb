266ba7636af92e3f70a358dbdc4253443b8874b2
Yousbaff <florianheymes@gmail.com>
2017-03-25 17:27:58 +0100

    Delete main.rb

--- main.rb
+++
@@ -1,9 +0,0 @@
-require "./divisibleParTrois"
-require "./DivisibleParCinq"
-require "./DivisibleParQuinze"
-
-100.each do | number|
-print number
-if (divParQuinze(number) = True ) do puts "Fiz Buz" 
-elsif (divParCinq(number) = True ) do puts "Buz" 
-elsif (divParTrois(number) = True ) do puts "Fiz" 
