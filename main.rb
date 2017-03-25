5c4542dc3588074de7bc3bc30e3549707f0502a7
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2017-03-25 17:48:55 +0100

    Fix conditions in main.rb

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require "./DivisibleParQuinze"
 
 100.each do | number|
 	print number
-	if (divParQuinze(number) = True ) do puts "Fiz Buz"; end
-	elsif (divParCinq(number) = True ) do puts "Buz"; end
-	elsif (divParTrois(number) = True ) do puts "Fiz"; end 
+	if (divParQuinze(number)) do puts "Fizz Buzz"; end
+	elsif (divParCinq(number)) do puts "Buzz"; end
+	elsif (divParTrois(number)) do puts "Fizz"; end 
 end
